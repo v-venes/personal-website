d714951f6989ac462a9aed643a401e4db98a11e7
Victor Neves <victor.neves4@fatec.sp.gov.br>
2022-03-09 21:48:02 -0300

    :tada: create project

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import HomePage from "./views/HomePage.svelte";
+  import { Button } from "sveltestrap";
+
+  export let name: string;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <HomePage />
+  <Button color="primary">Teste</Button>
+</main>
+
+<style>
+</style>
---
+++ src/views/HomePage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="homePage">
+  <p>aaaaaaah!</p>
+</div>
+
+<style>
+  .homePage {
+    background-color: orange;
+  }
+</style>
